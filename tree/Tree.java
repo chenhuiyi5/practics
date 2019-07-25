ba1894d37efd574b999ec27e3ed73a2dedf1a9f6
chenhuiyi5 <573030987@qq.com>
2019-07-25 15:59:37 +0800

    Create Tree.java

---
+++ tree/Tree.java
@@ -0,0 +1,32 @@
+package tree;
+/**
+ * 
+ * @author chy
+ * purpose:树对象的构造
+ *
+ */
+public class Tree {
+	private String root;
+	private Tree lCrild;
+	private Tree rChild;
+	public String getRoot() {
+		return root;
+	}
+	public void setRoot(String root) {
+		this.root = root;
+	}
+	public Tree getlCrild() {
+		return lCrild;
+	}
+	public void setlCrild(Tree lCrild) {
+		this.lCrild = lCrild;
+	}
+	public Tree getrChild() {
+		return rChild;
+	}
+	public void setrChild(Tree rChild) {
+		this.rChild = rChild;
+	}
+	
+
+}
